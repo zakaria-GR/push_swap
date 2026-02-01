b3a65a3bf3b3b8b2b64480f9b7faded888e6ba49
zakaria-GR <107518118+zakaria-GR@users.noreply.github.com>
2026-02-01 11:41:21 +0100

    Create has_duplicates.c

---
+++ has_duplicates.c
@@ -0,0 +1,19 @@
+int	has_duplicates(int *arr, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (arr[i] == arr[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
